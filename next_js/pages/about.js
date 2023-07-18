db592cf0fbb4c6509533ef86a2d0b1b071a7bcbd
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-07-18 20:25:34 +0100

    learn to create component and handle it as i want

---
+++ next_js/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head'
+
+const about = () => {
+  return (
+    <div>
+    <h1>About page</h1>     
+    </div>
+  )
+}
+
+export default about
--- next_js/pages/index.js
+++ next_js/pages/index.js
@@ -2,6 +2,12 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
+    <div>
+      <Head>
+        <title>Home page</title>
+      </Head>
       <h1>Wellcome anass </h1>
+    </div>
+
    )
 }
